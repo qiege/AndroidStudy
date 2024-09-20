e966771ad9cb8396fb7d50e11ba49f2b2bceabe1
litan.masker <litan.masker@bytedance.com>
2024-09-20 10:26:31 +0800

    新建java lib，可以运行java main

---
+++ Groovy/src/main/java/com/example/groovy/GroovyStarterMain.java
@@ -0,0 +1,8 @@
+package com.example.groovy;
+
+public class GroovyStarterMain {
+    public static void main(String[] args) {
+        System.out.println("Hello, Groovy!");
+        System.out.println("This is a test project.");
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AndroidStudy"
 include(":app")
- 
+include(":Groovy")
